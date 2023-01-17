c595d5dd34636d89d09cbedcb2875dcaf9bfc6e4
MtScience <107070506+MtScience@users.noreply.github.com>
2023-01-17 16:26:30 +0300

    Update Parser.hs

--- Parser.hs
+++ Parser.hs
@@ -1,44 +1,44 @@
 module Parser where
 
+-- Defines custom types for data and errors
+import Types (BFOperation(..))
+
+-- Necessary to build a parser
 import Text.Parsec
 import Text.Parsec.String
 
-data BFOperation = Print
-                 | Read
-                 | ShiftRight
-                 | ShiftLeft
-                 | Increment Int
-                 | Decrement Int
-                 | Loop [BFOperation]
-                 deriving Show
-
+{-
+Increment and decrement parser combinator. Since the are the same except for the sign
+and symbols used, the function is parametrized with the symbols to look for and the
+resulting value
+-}
 parseIncrement :: Char -> (Int -> BFOperation) -> Parser BFOperation
 parseIncrement sym op = (many1 $ char sym) >>= \inc -> return $ op $ length inc
 
+-- Shift parser combinator. Similar to parseIncrement
 parseShift :: Char -> BFOperation -> Parser BFOperation
 parseShift sym op = char sym >> return op
 
-parseRead :: Parser BFOperation
-parseRead = char ',' >> return Read
+-- IO parser combinator. Similar to the previous two
+parseIO :: Char -> BFOperation -> Parser BFOperation
+parseIO sym op = char sym >> return op
 
-parsePrint :: Parser BFOperation
-parsePrint = char '.' >> return Print
-
-
+-- Loop parser combinator. Thanks to the properties of Parsec, it automatically ensures
+-- that the brackets are balanced
 parseLoop :: Parser BFOperation
 parseLoop = do
   char '['
   loop <- parseBFcode `manyTill` (char ']')
   return $ Loop loop
 
-
+-- Comlete parser
 parseBFcode :: Parser BFOperation
 parseBFcode = parseIncrement '+' Increment
            <|> parseIncrement '-' Decrement
            <|> parseShift '>' ShiftRight
            <|> parseShift '<' ShiftLeft
-           <|> parseRead
-           <|> parsePrint
+           <|> parseIO ',' Read
+           <|> parseIO '.' Print
            <|> parseLoop
 
 readExpr :: String -> String
