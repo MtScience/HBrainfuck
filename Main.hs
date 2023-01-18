3f6ab19f1348248dc644705d34692885b40fcc03
MtScience <107070506+MtScience@users.noreply.github.com>
2023-01-18 15:23:42 +0300

    Update Main.hs

--- Main.hs
+++ Main.hs
@@ -1,8 +1,14 @@
 module Main where
 
+import Control.Monad.State
 import System.Environment
 
+import Evaluator (evaluate)
 import Parser (readExpr)
+import Types
 
 main :: IO ()
-main = getArgs >>= putStrLn . show . readExpr . head
+main = do
+  args <- getArgs
+  let ops = readExpr $ head args
+  evalStateT (mapM_ evaluate ops) (Tape [] 0 [])
