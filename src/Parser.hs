24813a4af61237c537fe39315fac52c5919d78e7
MtScience <107070506+MtScience@users.noreply.github.com>
2023-01-19 17:40:40 +0300

    Update Parser.hs

--- src/Parser.hs
+++ src/Parser.hs
@@ -4,37 +4,30 @@ import Prelude hiding (read)
 import Control.Monad.Except
 import Data.Word (Word8)
 
--- Necessary to build a parser
 import Text.Parsec
 import Text.Parsec.String (Parser)
 
--- Defines custom types for data and errors
 import Types
 
 
 stripComments :: String -> String
 stripComments = filter (`elem` "<>[]+-.,")
 
--- Increment and decrement parser combinators
 increment, decrement :: Parser BFOperation
 increment = Increment . fromIntegral . length <$> many1 (char '+')
 decrement = Decrement . fromIntegral . length <$> many1 (char '-')
 
--- Shift parser combinators. Similar to parseIncrement
 shiftl, shiftr :: Parser BFOperation
 shiftl = ShiftLeft <$ char '<'
 shiftr = ShiftRight <$ char '>'
 
--- IO parser combinators. Similar to the previous four
 read, write :: Parser BFOperation
 read  = Read <$ char ','
 write = Print <$ char '.'
 
--- Loop parser combinator
 loop :: Parser BFOperation
 loop = Loop <$> between (char '[') (char ']') code
 
--- Complete parser
 code :: Parser [BFOperation]
 code = many $ choice [increment, decrement, shiftl, shiftr, read, write, loop]
 
